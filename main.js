e6dda9710f116e786c1ad207f3d7a2fcaef00a53
Chaitra K J <chaitra@invictolabs.com>
2026-03-09 12:15:35 +0530

    package json update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const electron = require('electron');
-const { updateElectronApp } = require('update-electron-app');
-// updateElectronApp();
+// require('update-electron-app')();
+require("./index.js");
 
 let mainWindow;
 
@@ -17,25 +17,23 @@ function createWindow() {
         ? mainWindow.loadFile(path.join(__dirname, "views/order_search.html")) // Prod
         : mainWindow.loadURL("http://localhost:3000"); // Dev
 
-        mainWindow.on('ready-to-show', () => {
-            if (!mainWindow) {
-              throw new Error('"mainWindow" is not defined');
-          }
-          if (process.env.START_MINIMIZED) {
-              mainWindow.minimize();
-          } else {
-              mainWindow.show();
-          }
-      });
+    mainWindow.on('ready-to-show', () => {
+        if (!mainWindow) {
+            throw new Error('"mainWindow" is not defined');
+        }
+        if (process.env.START_MINIMIZED) {
+            mainWindow.minimize();
+        } else {
+            mainWindow.show();
+        }
+    });
 
-        mainWindow.on("closed", function () {
-            mainWindow = null;
-        });
+    mainWindow.on("closed", function () {
+        mainWindow = null;
+    });
 }
 
-require("./index.js");
-
-electron.app.on('on', () =>{ 
+electron.app.whenReady().then(() =>{
     createWindow();
 
     electron.app.on('activate', () => {
